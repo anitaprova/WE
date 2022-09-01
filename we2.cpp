51a93ee6c6470accff9ebca87477c92e2881c796
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-01 13:29:00 -0400

    fixed spaces

--- we2.cpp
+++ we2.cpp
@@ -21,7 +21,7 @@ int main()
 	int first_class_stamps = 100 * dollars / FIRST_CLASS_STAMP_PRICE;
 	int penny_stamps = 100 * dollars - first_class_stamps * FIRST_CLASS_STAMP_PRICE;
 	cout << "First class stamps: " << setw(6) << first_class_stamps << endl 
-		<< "Penny stamps:	" << setw(6) << penny_stamps << endl;
+		<< "Penny stamps:	    " << setw(6) << penny_stamps << endl;
 
 	return 0;
 }
