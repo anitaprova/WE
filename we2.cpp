b2fd7bd244b8c5d1ea4fab9c6857e70080d0301e
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-01 13:27:47 -0400

    fixed bugs

--- we2.cpp
+++ we2.cpp
@@ -7,6 +7,7 @@
  */
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -18,7 +19,7 @@ int main()
 	cin >> dollars;
 	
 	int first_class_stamps = 100 * dollars / FIRST_CLASS_STAMP_PRICE;
-	int penny_stamps = 100 * dollars - first_class_stamps * FIRST_CLASS_STAMP_ PRICE;
+	int penny_stamps = 100 * dollars - first_class_stamps * FIRST_CLASS_STAMP_PRICE;
 	cout << "First class stamps: " << setw(6) << first_class_stamps << endl 
 		<< "Penny stamps:	" << setw(6) << penny_stamps << endl;
 
