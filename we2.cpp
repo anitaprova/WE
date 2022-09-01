9e964614ae6125c28d1f01b2805679352075c770
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-01 13:26:32 -0400

    finished code

--- we2.cpp
+++ we2.cpp
@@ -1,1 +1,26 @@
+/*
+ * Author: Anita Prova
+ * Course: CSCI-135
+ * Instructor: Genady Maryash
+ * Assignment: Homework WE1
+ * The program prints the time it takes for the robot to travel the segment. 
+ */
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int FIRST_CLASS_STAMP_PRICE = 47;
+
+	cout << "Enter number of dollars: ";
+	int dollars;
+	cin >> dollars;
+	
+	int first_class_stamps = 100 * dollars / FIRST_CLASS_STAMP_PRICE;
+	int penny_stamps = 100 * dollars - first_class_stamps * FIRST_CLASS_STAMP_ PRICE;
+	cout << "First class stamps: " << setw(6) << first_class_stamps << endl 
+		<< "Penny stamps:	" << setw(6) << penny_stamps << endl;
+
+	return 0;
+}
